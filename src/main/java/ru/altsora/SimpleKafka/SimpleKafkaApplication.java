4b351501eb5cb2c861459da3f8fb872bc0db7633
altsora <vergun_a.s@mail.ru>
2021-02-08 23:31:12 +0300

    Конфигурация получателя

--- src/main/java/ru/altsora/SimpleKafka/SimpleKafkaApplication.java
+++ src/main/java/ru/altsora/SimpleKafka/SimpleKafkaApplication.java
@@ -1,18 +1,28 @@
 package ru.altsora.SimpleKafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import ru.altsora.SimpleKafka.model.UserDto;
 
 @EnableKafka
 @SpringBootApplication
 public class SimpleKafkaApplication {
 
 	@KafkaListener(topics="msg")
-	public void msgListener(String msg){
-		System.out.println("msg = " + msg);
+	public void msgListener(ConsumerRecord<Long, UserDto> record){
+		System.out.println("record.topic() = " + record.topic());
+		System.out.println("record.partition() = " + record.partition());
+		System.out.println("record.key() = " + record.key());
+		System.out.println("record.value() = " + record.value());
 	}
+
+//	@KafkaListener(topics="msg")
+//	public void msgListener(String msg){
+//		System.out.println("msg = " + msg);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleKafkaApplication.class, args);
--- src/main/java/ru/altsora/SimpleKafka/config/KafkaProducerConfig.java
+++ src/main/java/ru/altsora/SimpleKafka/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package ru.altsora.SimpleKafka.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,7 +17,10 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
-    private String kafkaServer = "localhost:9092";
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServer;
+//    private String kafkaServer = "localhost:9092";
 
     @Bean
     public Map<String, Object> producerConfigs() {
